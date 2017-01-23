84a1f48e3eeafc29a3b0a7beb27fd3da3419bdaa
Val Saven <val.saven@gmail.com>
2017-01-23 15:42:33 +0300

    feat: Add Header component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 
+import Header from '../Header';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Link to="/">
-          <header>
-            <h1>Dark Souls Guidebook</h1>
-          </header>
+          <Header/>
         </Link>
         {this.props.children}
       </div>
---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <header className="header">
+        <h1>Dark Souls Guidebook</h1>
+      </header>
+    );
+  }
+}
