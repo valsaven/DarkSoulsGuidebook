301ca16294067c31ad2a072a57a002dcff453ef7
Val Saven <val.saven@gmail.com>
2017-08-24 03:34:59 +0300

    feat: Create main menu

--- src/components/App/App.vue
+++ src/components/App/App.vue
@@ -1,8 +1,19 @@
 <template>
   <div class="App" id="app">
     <Header></Header>
+    <div class="main-menu">
+      {{item}}
+      <a v-for="item in menuItems" :src="item.alt">
+          <div class="content-item">
+            <img :src="item.img" :alt="item.alt"/>
+          <p>{{item.title}}</p>
+          </div>
+      </a>
+    </div>
   </div>
 </template>
+
+<style src="./style.css"></style>
 
 <script>
   import Vue from 'vue';
@@ -14,7 +25,18 @@
       'Header': Header
     },
     data () {
-      return {}
+      return {
+        menuItems: [
+          { title: 'CHARACTERS', img: '', alt: 'characters' },
+          { title: 'WEAPONS', img: '', alt: 'weapons' },
+          { title: 'ARMOR', img: '', alt: 'armor' },
+          { title: 'MAGIC', img: '', alt: 'magic' },
+          { title: 'ITEMS', img: '', alt: 'items' },
+          { title: 'ONLINE PLAY', img: '', alt: 'online' },
+          { title: 'STATS', img: '', alt: 'stats' },
+          { title: 'LOCATIONS', img: '', alt: 'locations' },
+        ],
+      }
     }
   }
 </script>
