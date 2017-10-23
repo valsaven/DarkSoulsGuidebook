0457bcc049134b8e6d48459b26a30ed54966efc4
Val Saven <val.saven@gmail.com>
2017-10-23 17:08:52 +0300

    Add homepage

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+// import HelloWorld from '@/components/HelloWorld';
+import Menu from '@/components/Menu/Menu';
 
 Vue.use(Router);
 
@@ -8,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: HelloWorld,
+      name: 'Menu',
+      component: Menu,
     },
   ],
 });
